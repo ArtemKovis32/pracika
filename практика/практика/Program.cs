9cb1790569cf5bcf52c94f36078be97d2e151d72
ArtemKovis32 <95204502+ArtemKovis32@users.noreply.github.com>
2021-12-04 11:27:59 +0500

    Add files via upload
    я давабил п

---
+++ практика/практика/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace практика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввод имени");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Ввод фамилии");
+            string? name2 = Console.ReadLine();
+            Console.WriteLine($"Здравия Желаю!{name}{name2}");;
+            Console.WriteLine("Песню запевай!");
+            Console.WriteLine(" ");
+            Console.WriteLine("Здравствуй,небо в облаках,");
+            Console.WriteLine("Здравствуй, юность в сапогах,");
+            Console.WriteLine("Пропади, моя тоска,");
+            Console.WriteLine("Вот он - я,привет,войска!");
+            Console.WriteLine("Эх,рельсы,поезда,");
+            Console.WriteLine("Как я попал сюда?");
+            Console.WriteLine(" ");
+            Console.WriteLine("Здесь не то, что на гражданке,");
+            Console.WriteLine("На какой-нибуть гражданке,");
+            Console.WriteLine("Жизнь с наружи и с изнанки");
+            Console.WriteLine("Сам попробуй изучи.");
+            Console.WriteLine("Для печали нет причин.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Непросто быть собой,");
+            Console.WriteLine("Когда шагает стой.");
+            Console.WriteLine("Только сердце птицей бьётся,");
+            Console.WriteLine("И ликует, и смеётся,");
+            Console.WriteLine("И ему не удаётся");
+            Console.WriteLine("Под комвоем петь в груди.");
+            Console.WriteLine("Знать бы, что там впереди.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Где-то течёт река,");
+            Console.WriteLine("Где-то дом, где всё ждут нас назад.");
+            Console.WriteLine("Это не грусть слегка,");
+            Console.WriteLine("Просто ветер щекочет глаза.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Где-то течёт река,");
+            Console.WriteLine("Где-то дом, где всё ждут нас назад.");
+            Console.WriteLine("Это не грусть слегка,");
+            Console.WriteLine("Просто ветер щекочет глаза.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Шаг вперёд и два назад,");
+            Console.WriteLine("Кто бы знал, чему я рад?");
+            Console.WriteLine("Просто сбросил я печаль");
+            Console.WriteLine("Словно голову с плеча.");
+            Console.WriteLine("На стыках рельсов путь,");
+            Console.WriteLine("Виски вбивает пульс.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Ты поймёшь, как будет нужно,");
+            Console.WriteLine("Где предательство, где дружба,");
+            Console.WriteLine("Где карьера, а где служба,");
+            Console.WriteLine("И как сердце безоружно,");
+            Console.WriteLine("Как обманчива наружность,");
+            Console.WriteLine("Сколько звёзд и сколько лычек,");
+            Console.WriteLine("Сколько лиц, личин, обличий");
+            Console.WriteLine("И как мало в нас различий.");
+            Console.WriteLine(" ");
+            Console.WriteLine("Всем удачи, встретимся...");
+            Console.WriteLine("            НА");
+            Console.WriteLine("        МОР-ФЛО-ТЕ");
+            Console.ReadKey();
+        }
+    }
+}
